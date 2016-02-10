e7976beccee89540ca82d7d7535e17f0b7e243f4
phobicdotno <spamme.post@gmail.com>
2016-02-10 12:44:09 +0100

    5x5
    Added 5x5 graph

--- wordbrain_graphs.py
+++ wordbrain_graphs.py
@@ -113,3 +113,34 @@ def makeEndPoints4x4(pos1, pos2, pos3, pos4, pos5, pos6, pos7, pos8, pos9, pos10
              pos14: [pos14, pos1, pos2, pos3, pos4, pos5, pos6, pos7, pos8, pos9, pos10, pos11, pos12, pos13, pos15, pos16],
              pos15: [pos15, pos1, pos2, pos3, pos4, pos5, pos6, pos7, pos8, pos9, pos10, pos11, pos12, pos13, pos14, pos16],
              pos16: [pos16, pos1, pos2, pos3, pos4, pos5, pos6, pos7, pos8, pos9, pos10, pos11, pos12, pos13, pos14, pos15]}
+
+def make5x5graph(pos1, pos2, pos3, pos4, pos5, pos6, pos7, pos8, pos9, pos10, pos11, pos12, pos13, pos14, pos15, pos16):
+    """
+    Returns a dictionary with startnode as key.
+    First value is same as startnode, and the rest is neighbour nodes
+
+     pos1  -   pos2  -   pos3  -   pos4
+      |    X     |   X     |   X     |
+     pos5  -   pos6  -   pos7  -   pos8
+      |    X     |   X     |   X     |
+     pos9  -  pos10  -  pos11  -   pos12
+      |    X     |   X     |   X     |
+    pos13  -  pos14  -  pos15  -   pos16
+
+    """
+    return { pos1: [pos1, pos2, pos6, pos5],
+             pos2: [pos2, pos1, pos3, pos7, pos6, pos5],
+             pos3: [pos3, pos2, pos4, pos8, pos7, pos6],
+             pos4: [pos4, pos3, pos8, pos7],
+             pos5: [pos5, pos1, pos2, pos6, pos10, pos9],
+             pos6: [pos6, pos1, pos2, pos3, pos7, pos11, pos10, pos9, pos5],
+             pos7: [pos7, pos2, pos3, pos4, pos8, pos12, pos11, pos10, pos6],
+             pos8: [pos8, pos3, pos4, pos12, pos11, pos7],
+             pos9: [pos9, pos5, pos6, pos10, pos14, pos13],
+            pos10: [pos10, pos5, pos6, pos7, pos11, pos15, pos14, pos13, pos9],
+            pos11: [pos11, pos6, pos7, pos8, pos12, pos16, pos15, pos14, pos10],
+            pos12: [pos12, pos7, pos8, pos16, pos15, pos11],
+            pos13: [pos13, pos9, pos10, pos14],
+            pos14: [pos14, pos9, pos10, pos11, pos15, pos13],
+            pos15: [pos15, pos10, pos11, pos12, pos16, pos14],
+            pos16: [pos16, pos11, pos12, pos15]}
