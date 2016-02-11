84996b91a8bcaff2a6881d342157aa71907cf0e3
phobicdotno <spamme.post@gmail.com>
2016-02-11 13:11:44 +0100

    Theoretically working version

--- wordbrain_calculate_5x5_size.py
+++ wordbrain_calculate_5x5_size.py
@@ -1,4 +1,4 @@
-# WORDBRAIN SOLUTION
+# WORDBRAIN - Generates all possible 5x5 paths
 
 import string
 import time
@@ -15,7 +15,6 @@ def find_all_paths_from_to(graph, start, end, path=[]):
     Finds all possible path from startnode to endnode
     Returns: A dictionary
     """
-    input_len = 3
     path = path + [start]
     if start == end:
         return [path]
@@ -29,25 +28,21 @@ def find_all_paths_from_to(graph, start, end, path=[]):
             for newpath in newpaths:
                 for char in newpath:
                     tempText+=''.join(char.lstrip('[]'))
-                    tempText = ''.join([i for i in tempText if not i.isdigit()])
-                # Remove all numbers before checking length of string
-                if len(tempText) >= input_len:
-                    paths.update({tempText: tempText})
-                    tempText = ''
+                paths.update({tempText: tempText})
+                tempText = ''
     return paths
 
 def show5x5pathsPossible(endpoints5x5, fiveGraph):
     all_paths = {}
-    tempPath={}
     for item in fiveGraph:
+        f = open('possibleFiveFrom-' + str(fiveGraph[item][0]) + '.txt', 'w')
         for i in range(1,len(endpoints5x5[item])):
-            tempPath(find_all_paths_from_to(fiveGraph,
-            fiveGraph[item][0], endpoints5x5[item][i]))
-            f = open('possibleFiveFrom-' + str(fiveGraph[item][0]) + '-to-' + str(endpoints5x5[item][i]) + '.txt', 'w')
-            json.dump(tempPath, f)
-            f.close()
-            tempPath={}
-
+            print('Started ' + str(item) + ' to ' + str(endpoints5x5[item][i]))         # How far we are
+            all_paths = find_all_paths_from_to(fiveGraph, fiveGraph[item][0], endpoints5x5[item][i])
+        print('Possible paths: ' + str(len(all_paths)))
+        json.dump(all_paths, f)
+        f.close()
+        print('Created: possibleFiveFrom-' + str(fiveGraph[item][0]) + '.txt\n' )
     return all_paths
 
 def manualInput5x5(wordlist, pos1, pos2, pos3, pos4, pos5, pos6, pos7,
