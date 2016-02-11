a59bc5ead7b134dfb8ec1ed92c33329c5ed6bef2
phobicdotno <spamme.post@gmail.com>
2016-02-11 09:10:51 +0100

    Add functions for calculating 5x5

--- wordbrain_solution_2_3_4_works_slow.py
+++ wordbrain_solution_2_3_4_works_slow.py
@@ -99,6 +99,43 @@ def manualInput4x4(wordlist, pos1, pos2, pos3, pos4, pos5, pos6, pos7,
     json.dump(possibleFour, f)
     f.close()
 
+def manualInput5x5(wordlist, pos1, pos2, pos3, pos4, pos5, pos6, pos7,
+pos8, pos9, pos10, pos11, pos12, pos13, pos14, pos15, pos16, pos17,
+pos18, pos19, pos20, pos21, pos22, pos23, pos24, pos25):
+
+    possibleFive = {}
+
+    fiveGraph = make5x5graph(pos1, pos2, pos3, pos4, pos5, pos6, pos7,
+    pos8, pos9, pos10, pos11, pos12, pos13, pos14, pos15, pos16, pos17,
+    pos18, pos19, pos20, pos21, pos22, pos23, pos24, pos25)
+
+    endpoints5x5 = makeEndPoints5x5(pos1, pos2, pos3, pos4, pos5, pos6,
+    pos7, pos8, pos9, pos10, pos11, pos12, pos13, pos14, pos15, pos16,
+    pos17, pos18, pos19, pos20, pos21, pos22, pos23, pos24, pos25)
+
+    possibleFive = show5x5pathsPossible(endpoints5x5, fiveGraph)
+
+    f = open('possibleFour.txt', 'w')
+    json.dump(possibleFour, f)
+    f.close()
+
+    end_time = time.time() - start_time
+    print('\n' + 'Time used on all possible paths for 4x4: '), print(end_time)
+    start_time = time.time()
+    possibleFour = list(set(possibleFour))  #  Sets are unordered collections of distinct objects.
+    end_time = time.time() - start_time
+    print('\n' + 'Time used on removing duplicates for 4x4: '), print(end_time)
+    start_time = time.time()
+    possibleFour = list(set(possibleFour).intersection(set(wordlist)))  # Get all words found, that exist in the dictionary
+    end_time = time.time() - start_time
+    print('\n' + 'Time used on finding all words in dictionary: '), print(end_time)
+    print('\n', 'Words in Wordlist:'), print(sorted(possibleFour, key=len)) # Print list sorted by length        
+
+    f = open('solutionsFour.txt', 'w')
+    json.dump(possibleFour, f)
+    f.close()
+
+
 def inputchar(wordlist):
     char = []
     input_str = input('Skriv inn dine bokstaver:' + '\n')
