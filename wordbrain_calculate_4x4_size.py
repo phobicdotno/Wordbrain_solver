5f0c6fac07a25118674eda7799bded7352412e40
phobicdotno <spamme.post@gmail.com>
2016-02-11 10:38:15 +0100

    Generates text files from a-b
    possibleFourFrom-a-to-b.txt
    possibleFourFrom-a-to-c.txt
    possibleFourFrom-a-to-d.txt osv
    
    + Ignores text files.

---
+++ wordbrain_calculate_4x4_size.py
@@ -0,0 +1,59 @@
+# WORDBRAIN SOLUTION
+
+import string
+import time
+import codecs   # For UTF-8 imports
+import json     # For saving to file
+from wordbrain_graphs import *
+from wordbrain_good_code import *
+
+# variables
+wordlist = ''
+
+def find_all_paths_from_to(graph, start, end, path=[]):
+    """
+    Finds all possible path from startnode to endnode
+    Returns: A dictionary
+    """
+    path = path + [start]
+    if start == end:
+        return [path]
+    if not start in graph:
+        return []
+    paths = {}
+    tempText = ''
+    for node in graph[start]:
+        if node not in path:
+            newpaths = find_all_paths_from_to(graph, node, end, path)
+            for newpath in newpaths:
+                for char in newpath:
+                    tempText+=''.join(char.lstrip('[]'))
+                paths.update({tempText: tempText})
+                tempText = ''
+    return paths
+
+def show4x4pathsPossible(endpoints4x4, fourGraph):
+    all_paths = {}
+    for item in fourGraph:
+        for i in range(1,len(endpoints4x4[item])):
+            f = open('possibleFourFrom-' + str(fourGraph[item][0]) + '-to-' + str(endpoints4x4[item][i]) + '.txt', 'w')
+            all_paths = find_all_paths_from_to(fourGraph, fourGraph[item][0], endpoints4x4[item][i])
+            json.dump(all_paths, f)
+            f.close()
+    return all_paths
+
+def manualInput4x4(wordlist, pos1, pos2, pos3, pos4, pos5, pos6, pos7,
+    pos8, pos9, pos10, pos11, pos12, pos13, pos14, pos15, pos16):
+
+    possibleFour = {}
+    fourGraph = make4x4graph(pos1, pos2, pos3, pos4, pos5, pos6, pos7,
+    pos8, pos9, pos10, pos11, pos12, pos13, pos14, pos15, pos16)
+
+    endpoints4x4 = makeEndPoints4x4(pos1, pos2, pos3, pos4, pos5, pos6,
+    pos7, pos8, pos9, pos10, pos11, pos12, pos13, pos14, pos15, pos16)
+
+    start_time = time.time()
+    possibleFour = show4x4pathsPossible(endpoints4x4, fourGraph)
+
+
+manualInput4x4(wordlist, 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p')
