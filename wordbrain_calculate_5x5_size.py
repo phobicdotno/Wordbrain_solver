e8315e5b23a714caef9f1bd898f624245f52fef6
phobicdotno <spamme.post@gmail.com>
2016-02-11 08:49:26 +0100

    Last change

--- wordbrain_calculate_5x5_size.py
+++ wordbrain_calculate_5x5_size.py
@@ -1,10 +1,38 @@
+# WORDBRAIN SOLUTION
+
 import string
 import time
 import codecs   # For UTF-8 imports
 import json     # For saving to file
-from wordbrain_good_code import loadWords
+from wordbrain_graphs import *
+from wordbrain_good_code import *
+from wordbrain_create_paths import *
 
-wordlist = loadWords()      # Import dictionary from file to list
+# variables
+words=''
+wordlist = loadWords()
+
+def find_all_paths_from_to(graph, start, end, path=[]):
+    """
+    Finds all possible path from startnode to endnode
+    Returns: A dictionary
+    """
+    path = path + [start]
+    if start == end:
+        return [path]
+    if not start in graph:
+        return []
+    paths = {}
+    tempText = 0
+    for node in graph[start]:
+        if node not in path:
+            newpaths = find_all_paths_from_to(graph, node, end, path)
+            for newpath in newpaths:
+                for char in newpath:
+                    tempText += 1
+    return tempText
+
+
 
 def show5x5pathsPossible(endpoints5x5, fiveGraph):
     all_paths = {}
@@ -31,3 +59,4 @@ pos18, pos19, pos20, pos21, pos22, pos23, pos24, pos25):
     possibleFive = show5x5pathsPossible(endpoints5x5, fiveGraph)
 
 
+print(manualInput5x5(wordlist, 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','y','z'))
