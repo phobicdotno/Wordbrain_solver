336f824a7fde267e7db189997f5c214f36e42a1b
phobicdotno <spamme.post@gmail.com>
2016-02-14 18:04:49 +0100

    Extract the first 3 letter from dictionary

--- wordbrain_make_dict_from_txt.py
+++ wordbrain_make_dict_from_txt.py
@@ -3,17 +3,27 @@ from wordbrain_good_code import *
 import string
 import time
 import codecs   # For UTF-8 imports
-wordlist = loadWords()
+#wordlist = loadWords()
 wordDict = {}
-
-for words in wordlist:
-    wordDict.update({words:words})
-#    print(words)
-
-f = open('wordDict-2-9.txt','w', encoding='utf-8-sig')
-json.dump(wordDict, f)
-f.close()
+tempWord = ''
+wordDictStart3 = {}
 
 file = open('wordDict-2-9.txt', 'r', encoding='utf-8-sig')
-tempDict = json.load(file)
+wordlist = json.load(file)
+file.close()
+
+for words in wordlist:
+    if len(words) > 2:
+        for i in range(0,3):
+            tempWord += ''.join(words[i])
+        wordDictStart3.update({tempWord:tempWord})
+        tempWord = ''
+#    print(words)
+
+f = open('wordDictStart3.txt','w', encoding='utf-8-sig')
+json.dump(wordDictStart3, f)
+f.close()
+
+file = open('wordDictStart3.txt', 'r', encoding='utf-8-sig')
+wordDictStart3 = json.load(file)
 file.close()
